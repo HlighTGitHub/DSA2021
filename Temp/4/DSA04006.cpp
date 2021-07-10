a8030c06cffe13510f626c0432fc88b0f7ecf911
HlighTGitHub <hlight.mail@gmail.com>
2021-07-10 15:18:15 +0700

    fixed

--- Temp/4/DSA04006.cpp
+++ Temp/4/DSA04006.cpp
@@ -1,56 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define setIO() ios::sync_with_stdio(false); cin.tie(0);
-#define all(a) a.begin(),a.end()
-#define fill(x,b) memset((x),b,sizeof((x)))
-#define fill2D(x,r,c,b) memset((x),b, sizeof(x[0][0]) * r * c)
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<string> vs;
-typedef vector<ii> vpii;
-int test = 1 , MULTI_TEST = 1; 
-vector < ll > v;
-vector < ll > pw;
-ll n,l,r;
-ll power(ll a, ll b) {
-    if (b == 0) return 1;
-    if (b == 1) return a;
-    ll p = power(a, b >> 1);
-    if (b & 1) return a * p * p;
-    else return p * p;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pld pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define Modd (ll)(1e9 + 7)
+ 
+using namespace std;
+ 
+bool Multitests = 1;
+ll test = 1, n, m, k, l, r, x, y;
+string f;
+ 
+ll CAL(ll d)
+{
+    ll ans = 0;
+    for (int i = 0; i < f.size(); i++)
+        if (f[i] != '0' && d >= (1LL << i))
+            ans += (d - (1LL << i)) / (2LL << i) + 1;
+    return ans;
 }
-
-void Input(){
-     cin >> n >> l >> r;
-     v.clear();
-}   
-void Solve(){
-    while (n) {
-        v.push_back(n);
-        n >>= 1;
-    }
-    int k = v.size() - 1;
-    int ans = 0;
-    for (ll i = l ; i <= r; i++) {
-        if (i & 1) ans++;
-        else {
-            ll val = i;
-            //if (val > power(2, k)) val -= power(2, k);
-            while (val & (val - 1)) {
-                int x = upper_bound(pw.begin(), pw.end(), val) - pw.begin() - 1;
-                val -= pw[x];
-            }
-            ans += v[k - (lower_bound(pw.begin(), pw.end(), val) - pw.begin())] & 1;
-        }   
-    }
-    cout << ans << '\n';
+ 
+void Solve()
+{
+    cin >> n >> l >> r;
+    f = bitset<50>(n).to_string();
+    while (f.size() > 1 && f[0] == '0')
+        f.erase(f.begin());
+    cout << CAL(r) - CAL(l - 1) << endl;
 }
-int main(){
+ 
+int main()
+{
     setIO();
-    for (int i = 0 ; i < 50; i++) pw.push_back(power(2, i));
-    if(MULTI_TEST == 1) cin >> test;
-    for(int i = 1 ; i <= test ; i++){Input(); Solve();}
+ 
+    if (Multitests)
+        (cin >> test).ignore();
+    while (test--)
+        Solve();
     return 0;
 }
